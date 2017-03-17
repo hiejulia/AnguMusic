4cbad1eb3993e22ea79e3b86a1ecb368dfd4c1b2
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-17 17:32:05 +0200

    create entry point for the main service of the app :octocat:

---
+++ myapp/src/app/providers/music.service.ts
@@ -0,0 +1,48 @@
+import 'rxjs';
+import {Http,Response} from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Music} from './music';
+import {Observable} from 'rxjs/Observable';
+const UrlApi = '';
+
+@Injectable() 
+export class MusicService {
+    constructor(private http:Http){}
+    //get Music from api
+    public getListMusic():Observable<Music[]> {
+        
+        return this.http.get(UrlApi)
+            .map(value => {
+                return value.json().data;
+            })
+            //error
+            .catch(this.handleError)
+    }
+
+    //get a song from api by id
+
+    public getSong(id:string):Observable<any> {
+        let url=``;
+        return this.http.get(url)
+            .map(response => response.json())
+            //error
+            .catch(this.handleError);
+    }
+
+
+    //get lyric
+    public getLyric(urlLyric:string):Observable<string> {
+        return this.http.get(urlLyric)
+            .map(response => response.text())
+            //error
+            .catch(this.handleError);
+    }
+
+    //handleError
+
+    public handleError(error:Response){
+        console.error(error);
+        return Observable.throw(error.json().error || 'Error with server call');
+    }
+
+}
