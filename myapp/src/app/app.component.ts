9353f10571c49b687ed9e06687d77ebb98fc213e
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-03-19 17:16:11 +0200

    set up OAuth :see_no_evil:

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+
+
+declare var Auth0Lock;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  lock= new Auth0Lock('client_id','auth0_domain');
+  constructor(){}
   title = 'app works!';
+  //login
+
+  login(){
+    var hash =this.lock.parseHash();
+    if(hash){
+      if(hash.error){
+        console.error(`There was an error loggin in ${hash.error}`);
+      }
+      else {
+        this.lock.getProfile(hash.id_token,function(err, profile){
+            if(err){
+                console.error(err);
+                return;
+            }
+            //else
+            localStorage.setItem('profile',JSON.stringify(profile));
+            localStorage.setItem('id_token',hash.id_token);
+        });
+      }
+    }
+  }
+  //logout
+  logout() {
+    localStorage.removeItem('profile');
+
+    localStorage.removeItem('id_token');
+  }
+
+  loggedIn(){
+    return tokenNotExpired();  }
+
+
+
 }
